db64e59c6e423f707cdd2c9db8d1664ec2e95f9c
Jack Herrington <jherr@pobox.com>
2020-06-21 12:33:55 -0700

    Adding tests

--- packages/ab-manager/__tests__/VariantChooser.spec.js
+++ packages/ab-manager/__tests__/VariantChooser.spec.js
@@ -1,5 +1,33 @@
+import React from "react";
+import renderer from "react-test-renderer";
 import VariantChooser from "../src/VariantChooser";
 
-describe("VariantChooser test", () => {
-  it("Should pick a variant", () => {});
+it("Should pick variant A", () => {
+  const VariantA = () => <div>A</div>;
+  const tree = renderer
+    .create(
+      <VariantChooser
+        variations={{
+          a: VariantA,
+        }}
+        variant="a"
+      />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("Should pick variant A", () => {
+  const VariantB = () => <div>B</div>;
+  const tree = renderer
+    .create(
+      <VariantChooser
+        variations={{
+          b: VariantB,
+        }}
+        variant="b"
+      />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
